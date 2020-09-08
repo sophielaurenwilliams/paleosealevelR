f24e91cb2e1768ebb34a7904e2847385d737fd69
Sophie Williams <70944011+sophielaurenwilliams@users.noreply.github.com>
2020-09-08 12:39:48 +0100

    Add files via upload
    Code to plot ITRAX data in ggplot2

---
+++ ITRAXcode.R
@@ -0,0 +1,16 @@
+#Load libraries and set theme
+library(tidyr)
+library(tidyverse)
+library(tidypaleo)
+theme_set(theme_bw(8))
+
+#Change data from wide to long format and tell R which columns to exclude
+
+ITRAX<-pivot_longer(ITRAX, -1, names_to="parameter", values_to = "value")
+
+ggplot(wapITRAX, aes(x = Depth , y = value))+
+  labs(x = "Depth (cm)")+
+  geom_line() +
+  coord_flip() +
+  scale_x_reverse() +
+  facet_wrap(vars(parameter), nrow=1, scales = "free_x")
